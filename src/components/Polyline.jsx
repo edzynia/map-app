f42803a585153c90b9d5ef46848e609f028a8289
Eugenia Dzynia <eugenia.dzynia@edaider.com>
2023-05-16 17:33:59 +0200

    updated markers

--- src/components/Polyline.jsx
+++ src/components/Polyline.jsx
@@ -1,24 +1,50 @@
 import { useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
-import 'leaflet-routing-machine';
+import 'leaflet/dist/leaflet.css';
 
 const Polyline = ({ positions }) => {
   const map = useMap();
 
   useEffect(() => {
     if (positions && positions.length > 0) {
-      const routingControl = L.Routing.control({
-        waypoints: [positions[0], positions[positions.length - 1]],
-        routeWhileDragging: true,
-        draggableWaypoints: false,
-        addWaypoints: false,
-      }).addTo(map);
+      // Create a green icon for the start marker
+      const startIcon = L.icon({
+        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
 
-      return () => {
-        routingControl.remove();
-      };
+      // Create a yellow icon for the end marker
+      const endIcon = L.icon({
+        iconUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41],
+      });
+
+      // Create the start marker with the green icon
+      const startMarker = L.marker(positions[0], { icon: startIcon });
+
+      // Create the end marker with the yellow icon
+      const endMarker = L.marker(positions[positions.length - 1], {
+        icon: endIcon,
+      });
+
+      // Create the polyline with the positions
+      const polyline = L.polyline(positions);
+
+      // Add the markers and the polyline to the map
+      startMarker.addTo(map);
+      endMarker.addTo(map);
+      polyline.addTo(map);
+
+      // Fit the map bounds to include both markers and the polyline
+      const bounds = L.latLngBounds(positions);
+      map.fitBounds(bounds);
     }
   }, [map, positions]);
 
